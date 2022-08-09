ad092f567f364da873de83b68a35627f8231b2f1
Gabriel Luppi <luppi.gabriel08@gmail.com>
2022-08-09 11:47:19 -0300

    Adicionando listener de pagamentos

--- src/main/java/com/luppi/kiyobank/controller/MovimentacaoController.java
+++ src/main/java/com/luppi/kiyobank/controller/MovimentacaoController.java
@@ -1,18 +1,15 @@
 package com.luppi.kiyobank.controller;
 
-import com.luppi.kiyobank.entity.Conta;
 import com.luppi.kiyobank.service.MovimentacaoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 @RestController
-@RequestMapping("movimentacao")
+@RequestMapping("/movimentacao")
 @RequiredArgsConstructor
 public class MovimentacaoController {
     private final MovimentacaoService movimentacaoService;
@@ -25,5 +22,15 @@ public class MovimentacaoController {
     @PostMapping("/deposito")
     public void depositar(Integer id, BigDecimal valor){
         movimentacaoService.depositar(id, valor);
+    }
+
+    @PostMapping("/transferencia")
+    public void transferir(Integer idPartida, Integer idDestino, BigDecimal valor) {
+        movimentacaoService.transferir(idPartida, idDestino, valor);
+    }
+
+    @PostMapping("/pagamento")
+    public void gerarPagamento(Integer idConta, BigDecimal valor) {
+        movimentacaoService.gerarPagamento(idConta, valor);
     }
 }
--- src/main/java/com/luppi/kiyobank/service/MovimentacaoService.java
+++ src/main/java/com/luppi/kiyobank/service/MovimentacaoService.java
@@ -4,6 +4,10 @@ import com.luppi.kiyobank.entity.Conta;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -22,6 +26,10 @@ public class MovimentacaoService {
     private String depositoTopic;
     @Value("${kafka.topic-transferencia}")
     private String transferenciaTopic;
+    @Value("${kafka.topic-pagamento}")
+    private String pagamentoTopic;
+    @Value("${kafka.topic-status-pagamento}")
+    private String statusPagamentoTopic;
 
     public void sacar(Integer id, BigDecimal valor){
         List<Conta> contas = conta.getContas();
@@ -30,8 +38,6 @@ public class MovimentacaoService {
 
         if(contaRecuperada.getSaldo().compareTo(valor) >= 0) {
             contaRecuperada.setSaldo(contaRecuperada.getSaldo().subtract(valor));
-            contas.remove(id);
-            contas.add(contaRecuperada);
             String mensagem = "SAQUE REALIZADO COM SUCESSO, conta -> " + contaRecuperada.getNumero() + " | " +
                     "titular -> " + contaRecuperada.getTitular() + " | valor -> " + valor + " R$";
 
@@ -46,12 +52,63 @@ public class MovimentacaoService {
 
         if(valor.compareTo(BigDecimal.ZERO) > 0) {
             contaRecuperada.setSaldo(contaRecuperada.getSaldo().add(valor));
-            contas.remove(id);
-            contas.add(contaRecuperada);
             String mensagem = "DEPOSITO REALIZADO COM SUCESSO, conta -> " + contaRecuperada.getNumero() + " | " +
                     "titular -> " + contaRecuperada.getTitular() + " | valor -> " + valor + " R$";
 
             produtorService.enviarMensagem(mensagem, saqueTopic);
         }
+    }
+
+    public void transferir(Integer idPartida, Integer idDestino, BigDecimal valor) {
+        List<Conta> contas = conta.getContas();
+        Conta contaPartida = contas.get(idPartida);
+        Conta contaDestino = contas.get(idDestino);
+
+        if(contaPartida.getSaldo().compareTo(valor) >= 0 && valor.compareTo(BigDecimal.ZERO) > 0) {
+            contaPartida.setSaldo(contaPartida.getSaldo().subtract(valor));
+            contaDestino.setSaldo(contaDestino.getSaldo().add(valor));
+
+            String mensagem = "TRANSFERENCIA REALIZADA COM SUCESSO, de -> " + contaPartida.getTitular() +
+                    " para " + contaDestino.getTitular() + ", no valor de " + valor + " R$";
+            produtorService.enviarMensagem(mensagem, transferenciaTopic);
+        }
+    }
+
+    public String gerarPagamento(Integer idConta, BigDecimal valor) {
+        String mensagem = "Pagamento gerado com sucesso: @" + idConta + "@" + valor;
+        produtorService.enviarMensagem(mensagem, pagamentoTopic);
+        return "Pagamento gerado com sucesso";
+    }
+
+    @KafkaListener(
+            topics = "${kafka.topic-pagamento}",
+            groupId = "group1",
+            containerFactory = "listenerContainerFactory",
+            clientIdPrefix = "topicoPagamento"
+    )
+    public boolean verificarDadosPagamento(@Payload String mensagem,
+                                           @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String key,
+                                           @Header(KafkaHeaders.OFFSET) Long offset) {
+
+        if(mensagem != null) {
+            String[] array;
+            array = mensagem.split("@");
+            int idCliente = Integer.parseInt(array[1]);
+            int valorPagamento = Integer.parseInt(array[2]);
+            BigDecimal valor = new BigDecimal(valorPagamento);
+
+            List<Conta> contasList = conta.getContas();
+            Conta contaRecuperada = contasList.get(idCliente);
+
+            if(contaRecuperada.getSaldo().compareTo(valor) >= 0 && valor.compareTo(BigDecimal.ZERO) >= 0) {
+                String statusMessage = "PAGAMENTO APROVADO";
+                produtorService.enviarMensagem(statusMessage, statusPagamentoTopic);
+                return true;
+            }
+            String statusMessage = "PAGAMENTO CANCELADO";
+            produtorService.enviarMensagem(statusMessage, statusPagamentoTopic);
+            return false;
+        }
+        return false;
     }
 }
